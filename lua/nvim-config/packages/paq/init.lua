f63f32d8fb9d2852ce1535c44732a86c78c162d1
k07e <sakk7e+dev@gmail.com>
2021-01-29 12:55:46 +0000

    Auto commit after yarn build

---
+++ lua/nvim-config/packages/paq/init.lua
@@ -0,0 +1,6 @@
+--[[ Generated with https://github.com/TypeScriptToLua/TypeScriptToLua ]]
+local ____exports = {}
+local ____paq = require("paq")
+local paq = ____paq.paq
+paq({"savq/paq-nvim", {opt = true}})
+return ____exports
